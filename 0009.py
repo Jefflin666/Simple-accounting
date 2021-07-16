61a5a81f4a1b99038fcca4ba2ec35ca3779bcebb
jeff lin <jeff1050930@gmail.com>
2021-07-16 15:31:55 +0800

    Second edition

--- 0009.py
+++ 0009.py
@@ -6,4 +6,8 @@ while True:
 	p=input('請輸入價格：')
 	commodity.append([c,p])
 for a in commodity:
-	print(a)
+	print(a[0],'的價格是',a[1],'元')
+with open ('test.csv','w')as f:
+	f.write('商品，價格\n')
+	for a in commodity:
+		f.write(a[0]+','+str(a[1])+'\n')
